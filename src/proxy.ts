3fec6cd87bb4408b3b668234d93895e4094e092a
Zinou <zinou@hmzonedz.com>
2026-04-26 18:11:10 +0100

    fix: update broken import in proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/session'
+import { updateSession } from '@/utils/supabase/middleware'
 
 export async function proxy(request: NextRequest) {
   return await updateSession(request)
